8c3a3ed348489a297eecfb41e42bbb2584f61ea3
skywalkerDarren <yh322yh@outlook.com>
2021-11-16 20:36:48 +0800

    build ui

--- lib/pages/home/home_screen.dart
+++ lib/pages/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:clash_dashboard_flutter/widgets/my_drawer.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,10 +6,9 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: FlutterLogo(
-        size: 50,
-      ),
+    return Scaffold(
+      appBar: AppBar(title: Text('主页'),),
+      drawer: MyDrawer(),
     );
   }
 }
---
+++ lib/widgets/my_drawer.dart
@@ -0,0 +1,34 @@
+import 'package:clash_dashboard_flutter/repository/clash_apis.dart';
+import 'package:clash_dashboard_flutter/repository/traffic_req.dart';
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          DrawerHeader(
+            child: Column(
+              children: [
+                Text('dashboard'),
+                StreamBuilder<TrafficResp>(
+                  stream: ClashApis().getTraffic(),
+                    builder: (context, snapshot) {
+                    return Text("up: ${(snapshot.data?.up ?? 0) / 1000}kb/s down: ${(snapshot.data?.down ?? 0) / 1000}kb/s");
+                }),
+              ],
+            ),
+            decoration: BoxDecoration(color: Theme.of(context).primaryColor),
+          ),
+          ListTile(title: Text("home"), onTap: () => Navigator.pop(context)),
+          ListTile(title: Text("proxies"), onTap: () => Navigator.pop(context)),
+          ListTile(title: Text("settings"), onTap: () => Navigator.pop(context)),
+          ListTile(title: Text("logs"), onTap: () => Navigator.pop(context)),
+        ],
+      ),
+    );
+  }
+}
