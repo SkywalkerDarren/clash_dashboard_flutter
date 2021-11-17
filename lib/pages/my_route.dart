42edc4dbd2f43c38bdae3076afe17573f818873e
skywalkerDarren <yh322yh@outlook.com>
2021-11-17 17:39:48 +0800

    format

--- lib/pages/my_route.dart
+++ lib/pages/my_route.dart
@@ -3,18 +3,18 @@ import 'package:clash_dashboard_flutter/pages/login/login_page.dart';
 import 'package:flutter/material.dart';
 
 final GlobalKey<NavigatorState> key = GlobalKey<NavigatorState>();
+
 class MyRouterDelegate extends RouterDelegate<List<RouteSettings>>
     with ChangeNotifier, PopNavigatorRouterDelegateMixin<List<RouteSettings>> {
-  
   static MyRouterDelegate? _instance;
+
   MyRouterDelegate._();
+
   factory MyRouterDelegate() {
     return _instance ??= MyRouterDelegate._();
   }
-  
-  final _pages = <Page>[
-    LoginPage()
-  ];
+
+  final _pages = <Page>[LoginPage()];
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +22,7 @@ class MyRouterDelegate extends RouterDelegate<List<RouteSettings>>
       key: navigatorKey,
       pages: List.of(_pages),
       onPopPage: (route, result) {
-        if(_pages.isNotEmpty) {
+        if (_pages.isNotEmpty) {
           _pages.removeLast();
         }
         notifyListeners();
@@ -33,7 +33,7 @@ class MyRouterDelegate extends RouterDelegate<List<RouteSettings>>
 
   @override
   GlobalKey<NavigatorState> get navigatorKey => key;
-  
+
   @override
   List<Page> get currentConfiguration => List.of(_pages);
 
@@ -47,12 +47,13 @@ class MyRouterDelegate extends RouterDelegate<List<RouteSettings>>
   }
 }
 
-class MyRouteInformationParser extends RouteInformationParser<List<RouteSettings>> {
-
+class MyRouteInformationParser
+    extends RouteInformationParser<List<RouteSettings>> {
   const MyRouteInformationParser() : super();
 
   @override
-  Future<List<RouteSettings>> parseRouteInformation(RouteInformation routeInformation) {
+  Future<List<RouteSettings>> parseRouteInformation(
+      RouteInformation routeInformation) {
     return Future.value([]);
   }
 
